8ff4d0d814b599607634573e6fc6f3741bb03974
GitHub Action <action@github.com>
2022-06-29 06:27:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/qchojr/intellijjenkinseditor/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/qchojr/intellijjenkinseditor/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.qchojr.intellijjenkinseditor.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.qchojr.intellijjenkinseditor.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/qchojr/intellijjenkinseditor/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.qchojr.intellijjenkinseditor.services
+
+import com.github.qchojr.intellijjenkinseditor.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
